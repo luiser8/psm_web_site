89db2efda9629910a8333faa03c203cfb1854920
Luis Rondon <leduardo.rondon@gmail.com>
2022-12-15 00:10:09 -0400

    maps_commit

--- components/Pricing.js
+++ components/Pricing.js
@@ -188,9 +188,9 @@ const Pricing = () => {
               <Testimoni />
             </motion.div>
           </ScrollAnimationWrapper>
-          <ScrollAnimationWrapper className="relative w-full mt-16">
+          <ScrollAnimationWrapper className="flex justify-center relative w-full mt-16">
             <motion.div variants={scrollAnimation} custom={{duration: 3}}>
-            <iframe width="1240" height="720" scrolling="no" src="https://maps.google.es/maps/ms?ie=UTF8&amp;hl=es-419&amp;oe=UTF8&amp;msa=0&amp;msid=202280427449831017393.0004ccaf6e66638aecf2c&amp;t=m&amp;ll=8.733077,-67.214355&amp;spn=10.410586,14.0625&amp;z=6&amp;output=embed"></iframe>
+            <iframe width="1180" height="720" scrolling="no" src="https://maps.google.es/maps/ms?ie=UTF8&amp;hl=es-419&amp;oe=UTF8&amp;msa=0&amp;msid=202280427449831017393.0004ccaf6e66638aecf2c&amp;t=m&amp;ll=8.733077,-67.214355&amp;spn=10.410586,14.0625&amp;z=6&amp;output=embed"></iframe>
             </motion.div>
           </ScrollAnimationWrapper>
         </div>
