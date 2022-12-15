baf04f5b3eef983d519af3901a9d2dc4cbc2a59a
Luis Rondon <leduardo.rondon@gmail.com>
2022-12-15 15:04:59 -0400

    Nuevas imagenes

--- components/Hero.js
+++ components/Hero.js
@@ -47,7 +47,7 @@ const Hero = ({
             <div className="flex w-full">
               <motion.div className="h-full w-full" variants={scrollAnimation}>
                 <Image
-                  src="/assets/Illustration1.png"
+                  src="/assets/Home1.png"
                   alt="VPN Illustrasi"
                   quality={100}
                   width={612}
