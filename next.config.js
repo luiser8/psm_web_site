a2be94bcce3665f61a3f4f567123ff26a8169a02
Luis Rondon <leduardo.rondon@gmail.com>
2023-01-19 10:11:17 -0400

    carousel web site images loading confg

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     images: {
-      formats: ['image/png', 'image/webp'],
+      formats: ['image/avif', 'image/webp'],
       remotePatterns: [
         {
           protocol: 'https',
