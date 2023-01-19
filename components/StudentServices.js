6e857c90a2f317c7acbbf7d76b900e622763da01
Luis Rondon <leduardo.rondon@gmail.com>
2023-01-19 09:44:21 -0400

    carousel web site images img with map

--- components/StudentServices.js
+++ components/StudentServices.js
@@ -190,7 +190,7 @@ const StudentServices = () => {
           </ScrollAnimationWrapper>
           <ScrollAnimationWrapper className="flex justify-center relative w-full mt-16">
             <motion.div variants={scrollAnimation} custom={{duration: 3}}>
-              {/* <iframe width="1380" height="720" scrolling="no" src="https://maps.google.es/maps/ms?ie=UTF8&amp;hl=es-419&amp;oe=UTF8&amp;msa=0&amp;msid=202280427449831017393.0004ccaf6e66638aecf2c&amp;t=m&amp;ll=8.733077,-67.214355&amp;spn=10.410586,14.0625&amp;z=6&amp;output=embed"></iframe> */}
+              <iframe width="1380" height="720" scrolling="no" src="https://maps.google.es/maps/ms?ie=UTF8&amp;hl=es-419&amp;oe=UTF8&amp;msa=0&amp;msid=202280427449831017393.0004ccaf6e66638aecf2c&amp;t=m&amp;ll=8.733077,-67.214355&amp;spn=10.410586,14.0625&amp;z=6&amp;output=embed"></iframe>
             </motion.div>
           </ScrollAnimationWrapper>
         </div>
