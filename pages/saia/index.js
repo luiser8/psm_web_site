daed075639a86247a57be827004b4adc5e437647
Luis E. Rondon <leduardo.rondon@gmail.com>
2023-04-13 14:17:09 -0400

    saia imagenes

--- pages/saia/index.js
+++ pages/saia/index.js
@@ -20,7 +20,7 @@ const Saia = () => {
               La Educación a distancia es una modalidad de enseñanza y aprendizaje cuya característica principal es la separación física de los participantes y facilitadores.
             </h3>
             <p className="mt-8 text-black-100">
-            S.A.I.A., está fundamentado en las ventajas que ofrece la educación en línea a través de las redes de computadoras, haciendo uso constante de las facilidades que ofrece Internet y otras tecnologías, lográndose así un ambiente educativo altamente colaborativo e interactivo donde el estudiante es el responsable de su ritmo de aprendizaje, estableciendo él mismo un horario a su conveniencia y sobre todo permitiéndole el acceso a una mayor cantidad de información actualizada y precisa en cualquier parte del mundo; lo que se traduce en el mejoramiento continuo en la calidad de la educación ofrecida por la Institución.
+              S.A.I.A., está fundamentado en las ventajas que ofrece la educación en línea a través de las redes de computadoras, haciendo uso constante de las facilidades que ofrece Internet y otras tecnologías, lográndose así un ambiente educativo altamente colaborativo e interactivo donde el estudiante es el responsable de su ritmo de aprendizaje, estableciendo él mismo un horario a su conveniencia y sobre todo permitiéndole el acceso a una mayor cantidad de información actualizada y precisa en cualquier parte del mundo; lo que se traduce en el mejoramiento continuo en la calidad de la educación ofrecida por la Institución.
             </p>
             {/* <ul className="text-black-500 self-start list-inside ml-8">
             {features.map((feature, index) => (
@@ -42,8 +42,8 @@ const Saia = () => {
           </ul> */}
             <div className="col-end-10 col-start-12 font-medium flex justify-start items-center mt-28">
               <Link href="/saia/detalle">
-              {/* href="https://saia2.psm.edu.ve" target="_blank" */}
-                <a  className="text-black-100 mx-2 sm:mx-4 tracking-wide hover:text-orange-400 transition-all">
+                {/* href="https://saia2.psm.edu.ve" target="_blank" */}
+                <a className="text-black-100 mx-2 sm:mx-4 tracking-wide hover:text-orange-400 transition-all">
                   Más detalles
                 </a>
               </Link>
@@ -53,7 +53,7 @@ const Saia = () => {
         <ScrollAnimationWrapper className="flex w-full justify-end">
           <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
             <Image
-              src="/assets/saia/saia_step_0.png"
+              src="/assets/saia/saia_home.png"
               alt="Academy"
               layout="responsive"
               quality={100}
