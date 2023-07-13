52f155fe10b5e5b29c00c407e257ee855a9c4d9a
Luis E. Rondon <leduardo.rondon@gmail.com>
2023-07-13 11:55:16 -0400

    Add evento con link

--- components/Layout/ImagesCarousel.js
+++ components/Layout/ImagesCarousel.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 import BackDrop from "./BackDrop";
 import Swipe from "react-easy-swipe";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import Image from "next/image";
+import ScrollAnimationWrapper from "./ScrollAnimationWrapper";
 
 function ImagesCarousel({ images, currentSlide, handleNextSlide, handlePrevSlide }) {
     const [loading, setLoading] = useState(true);
@@ -15,7 +19,7 @@ function ImagesCarousel({ images, currentSlide, handleNextSlide, handlePrevSlide
     }, []);
 
     return (
-        <div className="w-full h-[50vh] flex overflow-hidden relative m-auto">
+        <div className="w-full h-[66vh] flex overflow-hidden relative m-auto">
             <Swipe
                 onSwipeLeft={handleNextSlide}
                 onSwipeRight={handlePrevSlide}
@@ -25,13 +29,31 @@ function ImagesCarousel({ images, currentSlide, handleNextSlide, handlePrevSlide
                 {images.map((image, index) => {
                     if (index === currentSlide) {
                         return (
-                            <img
-                                key={index}
-                                src={image.url}
-                                layout="fill"
-                                className="h-auto w-auto transition-all"
-                                alt="Image for carousel"
-                            />
+                            // <a href={image.link} target={image.target}>
+                            //     <img
+                            //         key={index}
+                            //         src={image.url}
+                            //         layout="fill"
+                            //         className="h-auto w-auto transition-all"
+                            //         alt="Image for carousel"
+                            //     />
+                            // </a>
+                            <ScrollAnimationWrapper>
+                                <motion.div className="h-full w-full">
+                                    <Link key={image.link} legacyBehavior passHref href={image.link} target={image.target}>
+                                        <a target={`${image.link === "" ? '' : image.target}`} rel="noopener noreferrer" className={`${image.link === "" ? 'cursor-default' : 'cursor-pointer'}`}>
+                                            <Image
+                                                src={image.url}
+                                                height={660}
+                                                width={1920}
+                                                alt="Carousel"
+                                                layout="responsive"
+                                                quality={100}
+                                                objectFit="contain"/>
+                                        </a>
+                                    </Link>
+                                </motion.div>
+                            </ScrollAnimationWrapper>
                         );
                     }
                 })}
--- utils/mocks/institucionMock.js
+++ utils/mocks/institucionMock.js
@@ -8,18 +8,31 @@ export default [
         carousel: [
             {
                 id: 0,
-                url: '/assets/web1-min.png',
-                cloudinary: "https://res.cloudinary.com/luiser85/image/upload/v1674132064/web1_n55v2c.png",
+                url: '/assets/eventos/evento1.jpg',
+                cloudinary: "",
+                link: "https://docs.google.com/forms/d/e/1FAIpQLSeC-EAmUJhGm5eEX7U4lqpY6pnYFeS9elYMgGPSE-LmGdsyxw/viewform?pli=1",
+                target: "_blank"
             },
             {
                 id: 1,
-                url: '/assets/web2-min.png',
-                cloudinary: "https://res.cloudinary.com/luiser85/image/upload/v1674132079/web2_tnekov.png",
+                url: '/assets/web1-min.png',
+                cloudinary: "https://res.cloudinary.com/luiser85/image/upload/v1674132064/web1_n55v2c.png",
+                link: "",
+                target: "_blank"
             },
             {
                 id: 2,
+                url: '/assets/web2-min.png',
+                cloudinary: "https://res.cloudinary.com/luiser85/image/upload/v1674132079/web2_tnekov.png",
+                link: "",
+                target: "_blank"
+            },
+            {
+                id: 3,
                 url: '/assets/web3-min.png',
                 cloudinary: "https://res.cloudinary.com/luiser85/image/upload/v1674132071/web3_wrf39j.png",
+                link: "",
+                target: "_blank"
             },
         ]
     }
