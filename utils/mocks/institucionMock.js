d5641efcbd163a254bdfb1ac109a96008a7bcbe0
Luis E. Rondon <leduardo.rondon@gmail.com>
2023-07-18 08:47:54 -0400

    Agregar video iframe

--- utils/mocks/institucionMock.js
+++ utils/mocks/institucionMock.js
@@ -29,7 +29,7 @@ export default [
                 link: "https://www.youtube.com/embed/WNsJKB19ot8",
                 titleLink: "Ver transmisión en vivo",
                 target: "_self",
-                iframe: "https://www.youtube.com/embed/WNsJKB19ot8",
+                iframe: "https://www.youtube.com/embed/Vl1I-u8tmyY",
             },
             {
                 id: 2,
@@ -38,7 +38,7 @@ export default [
                 link: "https://www.youtube.com/watch?v=WNsJKB19ot8",
                 titleLink: "Ver transmision en vivo",
                 target: "_self",
-                iframe: "https://www.youtube.com/embed/WNsJKB19ot8",
+                iframe: "https://www.youtube.com/embed/Vl1I-u8tmyY",
                 //
             },
             // {
