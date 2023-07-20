b25be742bf10d693e27b6c2e9d0429edcce87473
Luis E. Rondon <leduardo.rondon@gmail.com>
2023-07-20 14:06:16 -0400

    Cambiar imagen evento3

--- utils/mocks/institucionMock.js
+++ utils/mocks/institucionMock.js
@@ -2,7 +2,7 @@ export default [
     {
         id: 1,
         name: "Institución",
-        image: "/assets/eventos/evento3-1.png",
+        image: "/assets/eventos/evento3-2.png",
         title: "Noticias del día",
         description: "¿Te apasiona la Tecnología y quieres conocer a otros que comparten tu interés? En la tarea de buscar la verdad y afianzar los valores trascendentes del hombre. Colaboramos en la orientación de la vida del país mediante el aporte doctrinario al esclarecimiento de los valores Nacionales.",
         description2: "Entonces no te pierdas el II Encuentro Tecnológico, una gran oportunidad de Aprendizajes adaptado a las Nuevas Tecnologías que se realizará desde el próximo 18 de julio, hasta el 21 de julio, a nivel Nacional e Internacional.",
@@ -36,7 +36,7 @@ export default [
                 url: '/assets/eventos/evento3.jpg',
                 cloudinary: "https://res.cloudinary.com/luiser85/image/upload/v1674132079/web2_tnekov.png",
                 link: "https://www.youtube.com/watch?v=WNsJKB19ot8",
-                titleLink: "Ver transmision en vivo",
+                titleLink: "Ver transmisión en vivo",
                 target: "_self",
                 iframe: "https://www.youtube.com/embed/Ya1zIBYsB9E",
                 //
