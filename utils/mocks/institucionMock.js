bdebbb84cd8293e5beb3f5fe452d4877ed29fe73
Luis E. Rondon <leduardo.rondon@gmail.com>
2023-09-21 12:30:38 -0400

    Agregar link youtube live x2 autoplay

--- utils/mocks/institucionMock.js
+++ utils/mocks/institucionMock.js
@@ -21,10 +21,10 @@ export default [
                 id: 0,
                 url: '/assets/eventos/evento2.jpeg',
                 cloudinary: "",
-                link: "https://www.youtube.com/embed/RVerIWFxiss?&autoplay=1",
+                link: "https://www.youtube.com/embed/PC-gKIIDWFE?&autoplay=1",
                 titleLink: "Ver transmisión en vivo",
                 target: "_self",
-                iframe: "https://www.youtube.com/embed/RVerIWFxiss?&autoplay=1",
+                iframe: "https://www.youtube.com/embed/PC-gKIIDWFE?&autoplay=1",
             },
             {
                 id: 1,
